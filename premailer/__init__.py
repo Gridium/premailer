6f000bf2197f987ecd9f1f6e1ac38eed7642eb10
Kimberly Nicholls <kimberly@gridium.com>
2014-05-02 08:32:11 -0700

    copy get_version stuff from boto3 so pip install works

--- premailer/__init__.py
+++ premailer/__init__.py
@@ -1,3 +1,17 @@
 from .premailer import Premailer, transform
 
 __version__ = '2.1.0'
+
+def get_version(full=False):
+    """
+    Returns a string-ified version number.
+
+    Optionally accepts a ``full`` parameter, which if ``True``, will include
+    any pre-release information. (Default: ``False``)
+    """
+    version = '.'.join([str(bit) for bit in __version__[:3]])
+
+    if full:
+        version = '-'.join([version] + list(__version__[3:]))
+
+    return version
--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import codecs
 import os.path
 import re
+import premailer
 
 # Prevent spurious errors during `python setup.py test`, a la
 # http://www.eby-sarna.com/pipermail/peak/2010-May/003357.html:
@@ -24,19 +25,9 @@ def md2stx(s):
 long_description = md2stx(long_description)
 
 
-def find_version(*file_paths):
-    version_file = codecs.open(os.path.join(os.path.dirname(__file__),
-                               *file_paths)).read()
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError("Unable to find version string.")
-
-
 setup(
     name='premailer',
-    version=find_version('premailer', '__init__.py'),
+    version=premailer.get_version(),
     description="Turns CSS blocks into style attributes",
     long_description=long_description,
     keywords='html lxml email mail style',
